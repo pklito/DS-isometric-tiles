5ad92b109d8d003e80b7f0a480330674b17d9b1c
Paulo Khayat <paulokhayat@gmail.com>
2023-11-20 01:47:34 +0100

    Remake tile byte storage

--- source/G_Isometric.c
+++ source/G_Isometric.c
@@ -4,25 +4,29 @@
  *  In charge of tile graphics
  */
 #include "G_Isometric.h"
-
-inline void _setSlice(u16* tiles, int tile, u8 color, TileSlices slice){
-	printf("set %d,%d,%d\n",tile,color,slice);
-	tiles[tile] &= !(0x7 << slice);
-	tiles[tile] ^= color << slice;
+typedef enum {
+	FACE_FLOOR = 0,
+	FACE_WALL_LEFT = 0b01,
+	FACE_WALL_RIGHT = 0b10
+} BlockFaces;
+inline void _setSlice(u16* tiles, int tile, TileSlices slice, u8 color,BlockFaces face){
+	tiles[tile] &= !((0b11111 << slice) | BIT(15));
+	tiles[tile] ^= ((face<<3) | color) << slice | BIT(15);
+}
+inline void _setWhole(u16* tiles, int tile, u8 color_top, BlockFaces face_top, u8 color_middle, BlockFaces face_middle, u8 color_bottom, BlockFaces face_bottom){
+	tiles[tile] = BIT(15) | ((face_bottom<<3|color_bottom)<<T_BOTTOM) | ((face_middle<<3|color_middle)<<T_MIDDLE)| ((face_top<<3|color_top)<<T_TOP) ;
 }
 void ISO_GenerateTiles(u16* tiles, s8* world, u8 world_dim_x, u8 world_dim_y, u8 world_dim_z){
 	int i,j,k;
-	printf("%p\n",world);
+
 	for(k = 0; k < world_dim_z; k++){
 		for(j = 0; j < world_dim_y; j++){
 			for(i = 0; i < world_dim_x; i++){
 
 				//if air, skip
+				u8 block_type = world[coords_3d(i,j,k,world_dim_x,world_dim_y)];
+				if(! block_type) continue;
 
-				if(! world[coords_3d(i,j,k,world_dim_x,world_dim_y)]) continue;
-				printf("%d,%d,%d, %d:%d\n",i,j,k, coords_3d(i,j,k,world_dim_x,world_dim_y),world[coords_3d(i,j,k,world_dim_x,world_dim_y)]);
-				u8 floor_color = 1;
-				u8 wall_color = 2;
 				//get topleft tile
 				int tile = ISO_convertWorldToTile(i,j,k);
 
@@ -30,36 +34,32 @@ void ISO_GenerateTiles(u16* tiles, s8* world, u8 world_dim_x, u8 world_dim_y, u8
 				bool is_full = ((i+j)%2 == 0);
 				if(is_full){
 					//TODO edge case of wrapping, edge case of original tile wrapping
-					_setSlice(tiles, tile, floor_color, T_MIDDLE);
-					_setSlice(tiles, tile++, wall_color,T_BOTTOM);
-					_setSlice(tiles, tile, floor_color, T_MIDDLE);
-					_setSlice(tiles, tile, wall_color,T_BOTTOM);
+					_setSlice(tiles, tile, T_MIDDLE, block_type, FACE_FLOOR);
+					_setSlice(tiles, tile++,T_BOTTOM, block_type,FACE_WALL_LEFT);
+					_setSlice(tiles, tile, T_MIDDLE, block_type, FACE_FLOOR);
+					_setSlice(tiles, tile, T_BOTTOM, block_type, FACE_WALL_RIGHT);
 
 					tile += TILES_SHAPE_WIDTH - 1;
 
-					_setSlice(tiles, tile, wall_color, T_TOP);
-					_setSlice(tiles, tile, wall_color, T_MIDDLE);
+					_setSlice(tiles, tile,T_TOP, block_type, FACE_WALL_LEFT);
+					_setSlice(tiles, tile,T_MIDDLE, block_type, FACE_WALL_LEFT);
 					tile += 1;
-					_setSlice(tiles, tile, wall_color, T_TOP);
-					_setSlice(tiles, tile, wall_color, T_MIDDLE);
+					_setSlice(tiles, tile,T_TOP, block_type, FACE_WALL_RIGHT);
+					_setSlice(tiles, tile,T_MIDDLE, block_type, FACE_WALL_RIGHT);
 				}
 				else{
 					//TODO edge case of wrapping, edge case of original tile wrapping
-					_setSlice(tiles, tile++, floor_color, T_BOTTOM);
-					_setSlice(tiles, tile, floor_color,T_BOTTOM);
+					_setSlice(tiles, tile++, T_BOTTOM, block_type, FACE_FLOOR); 	//++!
+					_setSlice(tiles, tile, T_BOTTOM, block_type, FACE_FLOOR);
 
 					tile += TILES_SHAPE_WIDTH - 1;
-					_setSlice(tiles, tile, floor_color, T_TOP);
-					_setSlice(tiles, tile, wall_color, T_MIDDLE);
-					_setSlice(tiles, tile, wall_color, T_BOTTOM);
+					_setWhole(tiles,tile,block_type, FACE_FLOOR, block_type, FACE_WALL_LEFT, block_type, FACE_WALL_LEFT);
 					tile += 1;
-					_setSlice(tiles, tile, floor_color, T_TOP);
-					_setSlice(tiles, tile, wall_color, T_MIDDLE);
-					_setSlice(tiles, tile, wall_color, T_BOTTOM);
+					_setWhole(tiles,tile,block_type, FACE_FLOOR, block_type, FACE_WALL_RIGHT, block_type, FACE_WALL_RIGHT);
 					tile += TILES_SHAPE_WIDTH - 1;
-					_setSlice(tiles, tile, wall_color, T_TOP);
+					_setSlice(tiles, tile, T_TOP, block_type, FACE_WALL_LEFT);
 					tile += 1;
-					_setSlice(tiles, tile, wall_color, T_TOP);
+					_setSlice(tiles, tile, T_TOP, block_type, FACE_WALL_RIGHT);
 				}
 			}
 		}
