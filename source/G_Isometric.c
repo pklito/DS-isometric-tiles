df91d5d95746e9a456b90aaf6f45ba8300559674
Paulo Khayat <paulokhayat@gmail.com>
2023-11-22 14:38:09 +0100

    Fix coords to tile

--- source/G_Isometric.c
+++ source/G_Isometric.c
@@ -20,6 +20,7 @@ inline void _setWhole(u16* tiles, int tile, u8 color_top, BlockFaces face_top, u
 void ISO_GenerateTiles(u16* tiles, s8* world, u8 world_dim_x, u8 world_dim_y, u8 world_dim_z){
 	int i,j,k;
 	u8 scale = 1;
+
 	for(k = 0; k < world_dim_z * scale; k++){
 		for(j = 0; j < world_dim_y * scale; j++){
 			for(i = 0; i < world_dim_x * scale; i++){
@@ -387,12 +388,14 @@ s16 ISO_convertWorldToTile(u8 px, u8 py, u8 pz){
 	int tile = TILES_ORIGIN;
 
 	//get the "floor coordinates" equivalent
-	s16 x = px - pz;
-	s16 y = py - pz;
+
+	s16 x = (s8)px;
+	s16 y = (s8)py;
+
 
 	//calculate the tile offset from the origin
 	int offset_x = (y - x);
-	int offset_y = (x+y)/2;
+	int offset_y = (x+y)/2 - pz;
 	tile += (offset_x + TILES_SHAPE_WIDTH * offset_y);
 
 	return tile;
